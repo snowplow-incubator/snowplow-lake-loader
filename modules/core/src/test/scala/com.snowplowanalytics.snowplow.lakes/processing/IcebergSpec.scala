323cbd0b977cd6aea9ca3524fd3f4592d8d7aade
Ian Streeter <ian@snowplowanalytics.com>
2023-12-15 20:37:41 +0000

    Integration tests for Hudi and Iceberg output formats (#38)

---
+++ modules/core/src/test/scala/com.snowplowanalytics.snowplow.lakes/processing/IcebergSpec.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2014-present Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This software is made available by Snowplow Analytics, Ltd.,
+ * under the terms of the Snowplow Limited Use License Agreement, Version 1.0
+ * located at https://docs.snowplow.io/limited-use-license-1.0
+ * BY INSTALLING, DOWNLOADING, ACCESSING, USING OR DISTRIBUTING ANY PORTION
+ * OF THE SOFTWARE, YOU AGREE TO THE TERMS OF SUCH LICENSE AGREEMENT.
+ */
+
+package com.snowplowanalytics.snowplow.lakes.processing
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import com.snowplowanalytics.snowplow.lakes.TestConfig
+
+import fs2.io.file.Path
+
+class IcebergSpec extends AbstractSparkSpec {
+
+  override def target: TestConfig.Target = TestConfig.Iceberg
+
+  /** Reads the table back into memory, so we can make assertions on the app's output */
+  override def readTable(spark: SparkSession, tmpDir: Path): DataFrame =
+    spark.sql("select * from test_catalog.test.events")
+
+  /** Spark config used only while reading table back into memory for assertions */
+  override def sparkConfig(tmpDir: Path): Map[String, String] =
+    Map(
+      "spark.sql.extensions" -> "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+      "spark.sql.catalog.test_catalog" -> "org.apache.iceberg.spark.SparkCatalog",
+      "spark.sql.catalog.test_catalog.type" -> "hadoop",
+      "spark.sql.catalog.test_catalog.warehouse" -> tmpDir.toString
+    )
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,6 +61,7 @@ object Dependencies {
   // spark and hadoop
   val sparkCore    = "org.apache.spark"           %% "spark-core"                % V.spark
   val sparkSql     = "org.apache.spark"           %% "spark-sql"                 % V.spark
+  val sparkHive    = "org.apache.spark"           %% "spark-hive"                % V.spark
   val delta        = "io.delta"                   %% "delta-core"                % V.delta
   val hudi         = "org.apache.hudi"            %% "hudi-spark3.4-bundle"      % V.hudi
   val iceberg      = "org.apache.iceberg"         %% "iceberg-spark-runtime-3.4" % V.iceberg
@@ -97,6 +98,17 @@ object Dependencies {
   val catsEffectTestkit = "org.typelevel" %% "cats-effect-testkit"        % V.catsEffect       % Test
   val catsEffectSpecs2  = "org.typelevel" %% "cats-effect-testing-specs2" % V.catsEffectSpecs2 % Test
 
+  val commonRuntimeDependencies = Seq(
+    delta        % Runtime,
+    hudi         % Runtime,
+    iceberg      % Runtime,
+    hadoopClient % Runtime,
+    slf4j        % Runtime,
+    protobuf     % Runtime,
+    sparkHive    % Runtime,
+    hiveExec     % Runtime // Needed for hudi
+  )
+
   val coreDependencies = Seq(
     streamsCore,
     loaders,
@@ -114,17 +126,7 @@ object Dependencies {
     catsEffectSpecs2,
     catsEffectTestkit,
     slf4j % Test
-  )
-
-  val commonRuntimeDependencies = Seq(
-    delta        % Runtime,
-    hudi         % Runtime,
-    iceberg      % Runtime,
-    hadoopClient % Runtime,
-    slf4j        % Runtime,
-    protobuf     % Runtime,
-    hiveExec     % Runtime // Needed for hudi
-  )
+  ) ++ commonRuntimeDependencies
 
   val awsDependencies = Seq(
     kinesis,
