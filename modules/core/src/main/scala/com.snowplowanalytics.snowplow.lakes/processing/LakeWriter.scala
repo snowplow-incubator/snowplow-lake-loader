03422f85d072dd374de672a1c4236322cb5fe9a6
Ian Streeter <ian@snowplowanalytics.com>
2024-03-12 23:37:07 +0000

    Add AWS Glue as an Iceberg catalog
    This enables using Redshift or Athena to query Snowplow events as an
    external Iceberg table in S3.

--- modules/core/src/main/scala/com.snowplowanalytics.snowplow.lakes/processing/LakeWriter.scala
+++ modules/core/src/main/scala/com.snowplowanalytics.snowplow.lakes/processing/LakeWriter.scala
@@ -18,7 +18,7 @@ import org.apache.spark.sql.types.StructType
 
 import com.snowplowanalytics.snowplow.runtime.HealthProbe
 import com.snowplowanalytics.snowplow.lakes.Config
-import com.snowplowanalytics.snowplow.lakes.tables.{DeltaWriter, HudiWriter, IcebergBigLakeWriter, IcebergHadoopWriter, Writer}
+import com.snowplowanalytics.snowplow.lakes.tables.{DeltaWriter, HudiWriter, IcebergWriter, Writer}
 
 trait LakeWriter[F[_]] {
 
@@ -38,10 +38,9 @@ object LakeWriter {
     target: Config.Target
   ): Resource[F, (LakeWriter[F], F[HealthProbe.Status])] = {
     val w = target match {
-      case c: Config.Delta          => new DeltaWriter(c)
-      case c: Config.Hudi           => new HudiWriter(c)
-      case c: Config.IcebergHadoop  => new IcebergHadoopWriter(c)
-      case c: Config.IcebergBigLake => new IcebergBigLakeWriter(c)
+      case c: Config.Delta   => new DeltaWriter(c)
+      case c: Config.Hudi    => new HudiWriter(c)
+      case c: Config.Iceberg => new IcebergWriter(c)
     }
     for {
       session <- SparkUtils.session[F](config, w)
